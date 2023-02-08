3090558adefaca41f39df7ff0c30c76eea59d419
gopheramit <amitwpu@gmail.com>
2023-02-08 22:39:02 +0530

    db setup

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
+
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,12 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  string
+	}
 }
 
 type application struct {
@@ -25,9 +36,17 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 4000, "api server port")
 	flag.StringVar(&cfg.env, "enc", "development", "Envrionment (developmetn|staging|production")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:pa55word@localhost/greenlight?sslmode=disable", "postgress")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open connections", 25, "postgres maximum open connection")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle connections", 25, "postgres maximum idle connection")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-time ", "15m", "postgres connection maximum idle time")
 	flag.Parse()
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
-
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 	app := &application{
 		config: cfg,
 		logger: logger,
@@ -43,6 +62,31 @@ func main() {
 		WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("starating %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+	db.SetConnMaxIdleTime(duration)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+
 }
